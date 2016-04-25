fb4db0a9d6420d3358beb1cefd0e8f7b8ea89a3a
spike <morndragon@126.com>
2016-04-25 22:15:29 +0800

    加油, 导航栏完成!

---
+++ components/Feed.js
@@ -0,0 +1,24 @@
+/**
+ * Created by wangchenlong on 16/4/25.
+ */
+
+'use strict';
+
+import React, {
+  Component,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+class Feed extends Component {
+  render() {
+    return (
+      <Text style={{flex:1}}>
+        HelloWorld!
+      </Text>
+    );
+  }
+}
+
+export default Feed;
--- index.ios.js
+++ index.ios.js
@@ -8,25 +8,45 @@ import React, {
   AppRegistry,
   Component,
   StyleSheet,
-  Text,
-  View
+  NavigatorIOS,
+  StatusBar
 } from 'react-native';
 
+import Feed from './components/Feed.js';
+
 class WclBBCNews extends Component {
+
+  _renderScene(route, navigator) {
+    var Component = route.component;
+    StatusBar.setBarStyle('light-content');
+    return (
+      <Component
+        {...route.props}
+        changeNavBarHeight={this.changeNavBarHeight}
+        navigator={navigator}
+        route={route}/>
+    );
+  }
+
+  componentWillMount() {
+    // 设置StatusBar的颜色, 默认default是黑色, light-content是白色
+    // 参考: https://facebook.github.io/react-native/docs/statusbar.html#barstyle
+    StatusBar.setBarStyle('light-content');
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <NavigatorIOS
+        style={{flex:1}}
+        translucent={false}
+        barTintColor={'#BB1919'}
+        titleTextColor={'white'}
+        tintColor={'white'}
+        initialRoute={{
+          component: Feed,
+          title: "Feed",
+          passProps: {}
+        }}/>
     );
   }
 }
@@ -36,18 +56,18 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F5FCFF'
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
+    margin: 10
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 5,
-  },
+    marginBottom: 5
+  }
 });
 
 AppRegistry.registerComponent('WclBBCNews', () => WclBBCNews);
