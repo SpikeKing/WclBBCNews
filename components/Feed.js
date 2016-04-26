9f3c8fbf4442cb62e6f2353a9aa8a32b3989ad31
spike <morndragon@126.com>
2016-04-26 08:34:44 +0800

    完成一个页面

--- components/Feed.js
+++ components/Feed.js
@@ -1,24 +1,133 @@
 /**
  * Created by wangchenlong on 16/4/25.
  */
-
 'use strict';
 
 import React, {
   Component,
   StyleSheet,
+  ListView,
+  View,
   Text,
-  View
+  TimerMixin,
+  RefreshControl
 } from 'react-native';
 
+import Loader from 'react-native-angular-activity-indicator';
+import Story from './Story.js';
+
 class Feed extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+    var dataSource = new ListView.DataSource({
+      rowHasChanged: (row1, row2) => row1 !== row2
+    });
+    this.state = {
+      dataSource: dataSource,
+      loaded: false,
+      isAnimating: true,
+      isRefreshing: false
+    };
+  }
+
+  componentDidMount() {
+    this._fetchData()
+  }
+
+  _fetchData() {
+    this.state.isRefreshing = true;
+
+    fetch(`http://trevor-producer-cdn.api.bbci.co.uk/content${this.props.collection || '/cps/news/world'}`)
+      .then((response) => response.json())
+      .then((responseData) => this._filterNews(responseData.relations))
+      .then((newItems) => {
+        this.setState({
+          dataSource: this.state.dataSource.cloneWithRows(newItems),
+          loaded: true,
+          isRefreshing: false,
+          isAnimating: false
+        })
+      }).done();
+  }
+
+  _filterNews(news = []) {
+    return new Promise((res, rej) => {
+      const filtered = news.filter(item => {
+        return item.content.format === 'bbc.mobile.news.format.textual'
+      });
+      res(filtered);
+    })
+  }
+
+  _renderLoading() {
     return (
-      <Text style={{flex:1}}>
-        HelloWorld!
-      </Text>
+      <View style={styles.loadingContainer}>
+        <Loader
+          isAnimating={this.state.isAnimating}
+          lineWidth={10}
+          color={'#FF00FF'}
+          style={styles.loadingView}
+          />
+      </View>
+    );
+  }
+
+  _renderStories(story) {
+    return (
+      <View>
+        <Text style={{flex:1}}>
+          HelloWorld!
+        </Text>
+      </View>
+    );
+  }
+
+  render() {
+
+    if (!this.state.loaded) {
+      return this._renderLoading();
+    }
+
+    return (
+      <ListView
+        testID={"Feed Screen"}
+        dataSource={this.state.dataSource}
+        renderRow={this._renderStories}
+        style={styles.listView}
+        contentInset={{top:0, left:0, bottom: 64, right: 0}}
+
+        scrollEventThrottle={200}
+        {...this.props}
+        refreshControl={
+          <RefreshControl
+            refreshing={this.state.isRefreshing}
+            onRefresh={this._fetchData()}
+            tintColor='#BB1919'
+            title="Loading..."
+            progressBackgroundColor="#FFFF00"
+          ></RefreshControl>
+        }
+        ></ListView>
     );
   }
 }
+
+var styles = StyleSheet.create({
+  loadingContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    flex: 1
+  },
+
+  loadingView: {
+    marginTop: 30,
+    marginRight: 50,
+    backgroundColor: '#FF00FF'
+  },
+
+  listView: {
+    backgroundColor: '#eee'
+  }
+});
 
 export default Feed;
---
+++ components/Story.js
@@ -0,0 +1,25 @@
+/**
+ * Created by wangchenlong on 16/4/26.
+ */
+'use strict';
+
+import React, {
+  Component,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+class Story extends Component {
+  render() {
+    return (
+      <View>
+        <Text style={{flex:1}}>
+          HelloWorld!
+        </Text>
+      </View>
+    );
+  }
+}
+
+export default Story;
